45cef20ce0fd7451bdb4320f807cf40bdf537347
Christopher Bradshaw <christopher.peter.bradshaw+github@gmail.com>
2018-01-24 00:00:44 -0800

    first submission!
    Didn't do well, but a good proof of concept

--- data-science-bowl-2018/data_loaders.py
+++ data-science-bowl-2018/data_loaders.py
@@ -5,18 +5,27 @@ import skimage.io
 datadir = "/home/christopher/Data/data/ml/data-science-bowl-2018/"
 
 def training_images():
-    keys = os.listdir(datadir + "train")
-    fnames = [datadir + "train/" + key + "/images/" + key + ".png" for key in keys]
+    return get_images("train")
+
+def test_images():
+    return get_images("test")
+
+def get_images(subdir):
+    if subdir not in set(["train", "test"]):
+        raise Exception("You probably want test or train images, not {}".format(subdir))
+    keys = os.listdir(datadir + subdir)
+    fnames = [datadir + subdir + "/" + key + "/images/" + key + ".png" for key in keys]
     res = []
     for img in skimage.io.imread_collection(fnames):
         assert len(img.shape) == 3
-        assert img.shape[2] == 4
-        assert img.shape[0] >= 256 and img.shape[1] >= 256
+        assert img.shape[0] >= 160 and img.shape[1] >= 160
+        # Could add some assertions here that A is boring (if it exists)
+        assert img.shape[2] in set([3, 4]) # RGB(A)
+
         img = np.rollaxis(img, 2, 0)
         img = np.mean(img[:3], axis=0).astype(np.uint8)
         res.append(img)
-
-    return np.array(res)
+    return res, keys
 
 def training_masks():
     keys = os.listdir(datadir + "train")
@@ -30,7 +39,7 @@ def training_masks():
             assert img.shape[0] >= 256 and img.shape[1] >= 256
             f_res.append(img)
         res.append(np.array(f_res))
-    return np.array(res)
+    return res
 
 # given masks returned from training_masks, returns them without any of the border masks
 # useful for stats
@@ -45,11 +54,11 @@ def filter_border_masks(masks):
             else: # no break
                 f_res.append(np.copy(mask))
         res.append(np.array(f_res))
-    return np.array(res)
+    return res
 
 # create a single mask for each image
 def single_masks(masks):
     res = []
     for image_masks in masks:
         res.append(np.sum(image_masks, axis=0))
-    return np.array(res)
+    return res
